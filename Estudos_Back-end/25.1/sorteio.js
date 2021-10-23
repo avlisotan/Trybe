947a11c1a05748ca2fbed9c24edf2845259971f8
Renato Silva <avlisotan@gmail.com>
2021-10-23 20:43:42 -0300

    Arquivo soteio

--- Estudos_Back-end/25.1/sorteio.js
+++ Estudos_Back-end/25.1/sorteio.js
@@ -1,9 +1,20 @@
 const readline = require('readline-sync');
 
-function sorteio() {
+function sorteioNumero() {
     const numero = readline.questionInt(`Descubra qual número pensei entre 1 e 5: `);
 
     const sorteio = Math.floor(Math.random() * (5-1) + 1);
+
+    validar(numero, sorteio);
+
+    const resultado =  readline.question(`Quer jogar novamente(S/N): `).toUpperCase();
+
+    if(resultado !== 'S') return console.log(`Obrigado e até a próxima!`);
+
+    sorteioNumero();
+}
+
+function validar(numero, sorteio) {
 
     if(numero != sorteio) {
         console.log(`Errrrooooou, era ${sorteio}.`);
@@ -13,4 +24,4 @@ function sorteio() {
     console.log(`Parabéns!!! Você acertou!!!`)
 }
 
-sorteio();
+sorteioNumero();
